d076028299e85991d7b2fda175fbf53749a19bb0
Adam Collins <acollins@alumni.ubc.ca>
2021-05-24 10:19:38 -0700

    Added some tests for node.go

--- internal/types/node_types.go
+++ internal/types/node_types.go
@@ -2,7 +2,6 @@ package types
 
 type NodeAddress string
 
-
 type INodeConnectionHandler interface {
 	Listen(address NodeAddress) error
 	Advertise(address NodeAddress, knownAddresses []NodeAddress) error
